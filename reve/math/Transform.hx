457c6681f1f17f66691949dc2a4a8fff8019b53f
novemberisms <briansarfati@gmail.com>
2019-12-07 16:08:46 +0800

    Rewrote Transform to be an abstract of h2d.col.Matrix

--- reve/math/Transform.hx
+++ reve/math/Transform.hx
@@ -1,26 +1,57 @@
 package reve.math;
 
-class Transform {
+import h2d.col.Matrix;
 
-    public var translation: Vector;
-    public var scale: Vector;
-    public var rotation: Float;
+abstract Transform(Matrix) from Matrix to Matrix {
 
-    public function new(?translation: Vector, ?scale: Vector, ?rotation: Float) {
-        this.translation = translation == null ? Vector.zero : translation;
-        this.scale = scale == null ? Vector.one : scale;
-        this.rotation = rotation == null ? 0 : rotation;
+    public var clone(get, never): Transform;
+
+    public function new() {
+        this = new Matrix();
     }
 
-    public inline function translated(translation: Vector): Transform {
-        return new Transform(this.translation + translation, scale, rotation);
+    public inline function scale(x: Float, y: Float): Transform {
+        final copy = clone;
+        copy.doScale(x, y);
+        return copy;
     }
 
-    public inline function scaled(scale: Vector): Transform {
-        return new Transform(translation, this.scale * scale, rotation);
+    public inline function translate(dx: Float, dy: Float): Transform {
+        final copy = clone;
+        copy.doTranslate(dx, dy);
+        return copy;
     }
 
-    public inline function rotated(rotation: Float): Transform {
-        return new Transform(translation, scale, this.rotation + rotation);
+    public inline function rotate(angle: Float): Transform {
+        final copy = clone;
+        copy.doRotate(angle);
+        return copy;
+    }
+
+    public inline function translateV(translation: Vector): Transform {
+        return translate(translation.x, translation.y);
+    }
+
+    public inline function doScale(x: Float, y: Float) {
+        this.scale(x, y);
+    }
+
+    public inline function doTranslate(dx: Float, dy: Float) {
+        this.translate(dx, dy);
+    }
+
+    public inline function doRotate(angle: Float) {
+        this.rotate(angle);
+    }
+
+    private inline function get_clone(): Transform {
+        final copy = new Matrix();
+        copy.a = this.a;
+        copy.b = this.b;
+        copy.c = this.c;
+        copy.d = this.d;
+        copy.x = this.x;
+        copy.y = this.y;
+        return copy;
     }
 }
--- reve/util/ObjectExtender.hx
+++ reve/util/ObjectExtender.hx
@@ -17,12 +17,4 @@ class ObjectExtender {
         obj.scaleY = scale.y;
         return scale;
     }
-
-    public static inline function applyTransform(obj: Object, transform: Transform) {
-        obj.x += transform.translation.x;
-        obj.y += transform.translation.y;
-        obj.scaleX *= transform.scale.x;
-        obj.scaleX *= transform.scale.y;
-        obj.rotation += transform.rotation;
-    }
 }
